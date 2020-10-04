db5df6b3e4f8718a7548d67df075583479d7f891
Arash Mohammadi <arash13131313@gmail.com>
2020-10-04 14:07:35 +0330

    adding TextField

---
+++ lib/src/helpers/ScarletInputDecoration.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// class ScarletInputDecoration extends InputDecoration {
+
+//   @override
+//   InputDecoration build(BuildContext context) {
+//     return InputDecoration();
+//   }
+
+// }
+
+const InputDecoration scarletInputDecoration = InputDecoration(
+    border: OutlineInputBorder(
+        borderSide: BorderSide(width: 20, color: Colors.black)));
